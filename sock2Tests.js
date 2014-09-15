97ff73e494a23af40085323d104c06caa999e5f3
Glenn <tglenn@bennington.edu>
2014-09-15 07:05:29 -0400

    got a puddle running, still need to make it pretty

--- sock2Tests.js
+++ sock2Tests.js
@@ -1,16 +1,33 @@
 var net = require('net');
-var client = net.connect({port: 8124},
+var readline = require("readline");
+var name = "FoxyPan";
+
+ioBar = (function() {
+	var bufferer = new Buffer("");
+	return (readline.createInterface({
+			input:process.stdin,
+			output:process.stdout
+	}))
+}());
+
+
+var client = net.connect({port: 8124,host:'10.10.117.114'},
     function() { //'connect' listener
   console.log('client connected');
-  client.write('world!\r\n');
 });
 
 client.on('data', function(data) {
-  console.log(data.toString());
-  client.end();
+  console.log("*** " + data.toString() + " ***");
 });
 
 client.on('end', function() {
   console.log('client disconnected');
 });
 
+ioBar.setPrompt(name+": ");
+ioBar.on('line', function (line){
+	bufferer = new Buffer(name+": "+line);
+	ioBar.prompt()
+	
+	client.write(bufferer);	
+});
--- sockTests.js
+++ sockTests.js
@@ -1,11 +1,24 @@
 var net = require('net');
+var conns = [];
 var server = net.createServer(function(c) { //'connection' listener
   console.log('server connected');
-  c.on('end', function() {
+  conns.push(c);
+	var num = conns.length -1;
+	console.log("NUM == " +  num);
+	c.on('end', function() {
     console.log('server disconnected');
   });
-  c.write('hello\r\n');
-  c.pipe(c);
+	
+	c.on('data', function(data) {
+		for (i in conns){
+			if (i != num){
+			conns[i].write(data);
+			}
+		}
+		});	
+	
+  c.write('Welcome to puddle 10.10.117.114\r\n');
+  //c.pipe(c);
 });
 server.listen(8124, function() { //'listening' listener
   console.log('server bound');
